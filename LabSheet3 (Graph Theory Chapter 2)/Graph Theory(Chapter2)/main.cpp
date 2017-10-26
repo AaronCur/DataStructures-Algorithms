b84021c6ab240d2f19d7fd88372788cf199ed793
AaronCur <c00202505@itcarlow.ie>
2017-10-26 10:27:56 +0100

    Lab3-1 complete

---
+++ LabSheet3 (Graph Theory Chapter 2)/Graph Theory(Chapter2)/main.cpp
@@ -0,0 +1,47 @@
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include "Graph.h"
+
+using namespace std;
+
+
+typedef GraphArc<std::string, int> Arc;
+typedef GraphNode<std::string, int> Node;
+
+void visit( Node * node ) {
+	cout << "Visiting: " << node->data() << endl;
+}
+
+int main(int argc, char *argv[]) {
+
+    Graph<std::string, int> graph( 25 );
+	
+	std::string nodeLabel;
+	int i = 0;
+	ifstream myfile;
+	myfile.open ("nodes.txt");
+
+	while ( myfile >> nodeLabel ) {
+		graph.addNode(nodeLabel, i++);
+	}
+
+	myfile.close();
+	myfile.open("arcs.txt");
+
+	int from, to, weight;
+	while ( myfile >> from >> to >> weight ) {
+		graph.addArc(from, to, weight);
+	}
+    myfile.close();
+
+	// Now traverse the graph.
+	graph.breadthFirst( graph.nodeIndex(0), visit );
+	//graph.adaptedBreadthFirst( graph.nodeIndex(0), graph.nodeIndex(15) );
+	//Node *goal = graph.nodeIndex(15);
+
+	system("PAUSE");
+	
+}
